8fea30c698301aa2a3af3771bda0bf73b970f14f
Alex Turaev <altex_76@mail.ru>
2023-10-23 20:39:37 +0300

    first commit

---
+++ src/main/java/ru/notification/system/projectnotice/ProjectNoticeApplication.java
@@ -0,0 +1,13 @@
+package ru.notification.system.projectnotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNoticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectNoticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/notification/system/projectnotice/ProjectNoticeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.notification.system.projectnotice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNoticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
